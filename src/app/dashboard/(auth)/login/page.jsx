abab0d7c9ef98f605c5ffb646254cfbd91a0d0c8
fendishatech <liyukibir@gmail.com>
2023-06-04 11:48:48 -0700

    group route for pages

---
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <h1 className="text-5xl text-green-400 font font-extralight text-center">
+      Login
+    </h1>
+  );
+};
+
+export default Login;
---
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <h1 className="text-5xl text-green-400 font font-extralight text-center">
+      Register
+    </h1>
+  );
+};
+
+export default Register;
