c53531efff16d18e6337fe1c17db0fd0f08539ab
fendishatech <liyukibir@gmail.com>
2023-06-04 11:45:46 -0700

    dynamic routes

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogPost = () => {
+  return (
+    <h1 className="text-5xl text-green-400 font font-extralight text-center">
+      BlogPost
+    </h1>
+  );
+};
+
+export default BlogPost;
---
+++ src/app/portfolio/[category]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Category = () => {
+  return (
+    <h1 className="text-5xl text-green-400 font font-extralight text-center">
+      Category
+    </h1>
+  );
+};
+
+export default Category;
