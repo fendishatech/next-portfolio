ba8a4b1eb2fc938898503642fb8069f8f3bb34e4
fendishatech <liyukibir@gmail.com>
2023-06-04 22:34:56 -0700

    hero section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
 const About = () => {
   return (
-    <h1 className="text-5xl text-green-400 font font-extralight text-center">
-      About
-    </h1>
+    <div className="px-6 py-2 flex flex-col">
+      <Image
+        src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        fill={true}
+        alt="About"
+      />
+      <div className="flex justify-start flex-col">
+        <h1>About Us</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo
+          praesentium distinctio sint!
+        </p>
+      </div>
+    </div>
   );
 };
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,33 @@
 import Image from "next/image";
-
+import HeroImg from "@hero.png";
 export default function Home() {
   return (
-    <h1 className="text-5xl text-green-400 font font-extralight text-center">
-      High hopes lama!
-    </h1>
+    <>
+      <Hero />
+    </>
   );
 }
+
+const Hero = () => {
+  return (
+    <div className="w-full flex flex-col md:flex-row items-center gap-x-[100px]">
+      <div className="w-full h-full flex-1 flex flex-col p-6 mt-4 gap-y-6">
+        <h1 className="py-4 text-7xl font-bold bg-gradient-to-b from-green-500 via-green-400 to-slate-200 text-transparent bg-clip-text">
+          Better design your digital products.
+        </h1>
+        <p className="text-2xl text-gray-800">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium,
+          dolore.
+        </p>
+        <div>
+          <button className="px-8 py-4 text-2xl font-semibold text-white rounded bg-green-600 hover:bg-green-400">
+            Get Started
+          </button>
+        </div>
+      </div>
+      <div className="flex-1  p-6 mt-4">
+        <Image src={HeroImg} className="w-full object-cover" alt="Hero" />
+      </div>
+    </div>
+  );
+};
