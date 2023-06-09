c720bc1d5faf94320e3d0894234f3bf2dd58633b
fendishatech <liyukibir@gmail.com>
2023-06-09 08:42:59 -0700

    portfolio design

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -5,11 +5,11 @@ import React from "react";
 const Category = ({ params }) => {
   return (
     <div className="flex flex-col">
-      <h1 className="text-3xl text-green-400 font font-extralight text-start">
+      <h1 className="mb-6 text-3xl text-green-400 font font-extralight text-start">
         {params.category}
       </h1>
-      <div className="flex">
-        <div className="flex-1 gap-10 justify-center flex flex-col">
+      <div className="flex gap-4">
+        <div className="flex-1 gap-8 justify-center flex flex-col">
           <h2 className=" text-2xl font-bold">Title</h2>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam
@@ -22,6 +22,7 @@ const Category = ({ params }) => {
         </div>
         <div className="h-[400px] flex-1 relative">
           <Image
+            className="object-cover"
             src="https://images.pexels.com/photos/627901/pexels-photo-627901.jpeg?auto=compress&cs=tinysrgb&w=400"
             fill={true}
             alt={params.category}
