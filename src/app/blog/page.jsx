562f65e84deb7e49a50a590413e680a46d9d2a9f
fendishatech <liyukibir@gmail.com>
2023-06-10 06:09:36 -0700

    blogs

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,60 @@
+import PrimaryButton from "@/components/button/PrimaryButton";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Blog = () => {
   return (
-    <h1 className="text-5xl text-green-400 font font-extralight text-center">
-      Blog
-    </h1>
+    <div className="p-6 flex flex-col gap-6">
+      <h1 className="text-3xl text-green-400 font font-extralight text-start">
+        Blogs
+      </h1>
+      {/* card */}
+      <Link href={"/blog/1"}>
+        <div className="flex gap-4 hover:shadow-lg">
+          <div className="h-[400px] flex-1 relative">
+            <Image
+              className="object-cover"
+              src="https://images.pexels.com/photos/627901/pexels-photo-627901.jpeg?auto=compress&cs=tinysrgb&w=400"
+              fill={true}
+              alt={"Blog"}
+            />
+          </div>
+          <div className="flex-1 gap-8 justify-center flex flex-col">
+            <h2 className=" text-4xl font-bold">Title</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam
+              illo, iusto dolore necessitatibus numquam recusandae voluptatum
+              rem repudiandae.
+            </p>
+          </div>
+        </div>
+      </Link>
+      {/* end card */}
+      {/* card */}
+      <div className="flex gap-4">
+        <div className="h-[400px] flex-1 relative">
+          <Image
+            className="object-cover"
+            src="https://images.pexels.com/photos/627901/pexels-photo-627901.jpeg?auto=compress&cs=tinysrgb&w=400"
+            fill={true}
+            alt={"Blog"}
+          />
+        </div>
+        <div className="flex-1 gap-8 justify-center flex flex-col">
+          <h2 className=" text-4xl font-bold">Title</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam
+            illo, iusto dolore necessitatibus numquam recusandae voluptatum rem
+            repudiandae.
+          </p>
+          <div className="">
+            <PrimaryButton url={"/"} text={"Read More"} />
+          </div>
+        </div>
+      </div>
+      {/* end card */}
+    </div>
   );
 };
 
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -4,13 +4,36 @@ import React from "react";
 
 const Category = ({ params }) => {
   return (
-    <div className="flex flex-col">
-      <h1 className="mb-6 text-3xl text-green-400 font font-extralight text-start">
+    <div className="flex flex-col gap-6">
+      <h1 className="text-3xl text-green-400 font font-extralight text-start">
         {params.category}
       </h1>
-      <div className="flex gap-4">
+      {/* card */}
+      <div className="flex flex-row-reverse gap-4">
         <div className="flex-1 gap-8 justify-center flex flex-col">
-          <h2 className=" text-2xl font-bold">Title</h2>
+          <h2 className=" text-4xl font-bold">Title</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam
+            illo, iusto dolore necessitatibus numquam recusandae voluptatum rem
+            repudiandae.
+          </p>
+          <div className="">
+            <PrimaryButton url={"/"} text={"Read More"} />
+          </div>
+        </div>
+        <div className="h-[400px] flex-1 relative">
+          <Image
+            className="object-cover"
+            src="https://images.pexels.com/photos/627901/pexels-photo-627901.jpeg?auto=compress&cs=tinysrgb&w=400"
+            fill={true}
+            alt={params.category}
+          />
+        </div>
+      </div>
+      {/* end card */}
+      <div className="flex  gap-4">
+        <div className="flex-1 gap-8 justify-center flex flex-col">
+          <h2 className=" text-4xl font-bold">Title</h2>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam
             illo, iusto dolore necessitatibus numquam recusandae voluptatum rem
